c850feb049bce285afe7a6c3efa5235d86e69760
HOTGMAC05 <hotgmac05@HOTGMAC05.local>
2024-07-02 07:53:22 +0800

    remove unwanted imports

--- App.tsx
+++ App.tsx
@@ -7,23 +7,7 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Text, View} from 'react-native';
 
 type SectionProps = PropsWithChildren<{
   title: string;
